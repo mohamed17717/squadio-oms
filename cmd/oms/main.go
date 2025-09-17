e2de79565ef8378903ed7c0100f8ada618dc59ed
mhmd <d3v.mhmd@gmail.com>
2025-09-17 11:09:51 +0300

    [Main] start db/ migrate/ indexs/ and start gin server

--- cmd/oms/main.go
+++ cmd/oms/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"oms-services/config"
+	"oms-services/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	db := config.ConnectDatabase()
+
+	// Auto migrate all models
+	err := models.AutoMigrate(db)
+	if err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+
+	// Create indexes
+	err = models.CreateIndexes(db)
+	if err != nil {
+		log.Fatal("Index creation failed:", err)
+	}
+
+	// apis.AuthRoutes()
+	// apis.ActionRoutes()
+	// apis.SubscriberRoutes()
+	// apis.UserConfigurationRoutes()
+	// apis.WebhookLogRoutes()
+	// apis.SubscriberActionRoutes()
+	// apis.ChangeRoutes()
+	// Start the Gin server
+	gin.SetMode(gin.DebugMode)
+	config.Server.Run()
 }
